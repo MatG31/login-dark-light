bd4c0e5e1babef77bdd001bbba452845fafe7bb9
MatG31 <matheusgameleira31@gmail.com>
2025-04-19 18:57:33 -0300

    mudar cores e notificar mudança de modo

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@
 
 const mode = document.getElementById("mode__icon");
 const form = document.getElementById("login__form");
+const mensagemClara = document.getElementById("mensagem-modo-claro");
+const mensagemEscura = document.getElementById("mensagem-modo-escuro");
 
 mode.addEventListener("click", () => {
   if (mode.classList.contains("fa-moon")) {
@@ -18,3 +20,28 @@ mode.addEventListener("click", () => {
   mode.classList.add("fa-moon");
   form.classList.remove("dark");
 });
+
+function notificarModo(){
+
+if(form.classList.contains("dark") && mensagemEscura.classList.contains("oculto")){
+  
+  mensagemClara.classList.remove("oculto");
+  mensagemClara.classList.add("mensagem-modo-claro");
+  setTimeout(() => {
+    mensagemClara.classList.remove("mensagem-modo-claro");
+    mensagemClara.classList.add("oculto");
+  }, 800);
+
+}else if(mensagemClara.classList.contains("oculto")){
+  
+  mensagemEscura.classList.remove("oculto");
+  mensagemEscura.classList.add("mensagem-modo-escuro");
+  setTimeout(() => {
+    mensagemEscura.classList.remove("mensagem-modo-escuro");
+    mensagemEscura.classList.add("oculto");
+  }, 800);
+  
+}
+
+}
+
